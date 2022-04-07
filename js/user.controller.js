7c8ef5360b6b7cea6001d2ad5dd445f256393911
Uri Baynesay <uri552@gmail.com>
2022-04-07 20:41:26 +0300

    added basic validation and functionality to user settings html

--- js/user.controller.js
+++ js/user.controller.js
@@ -1,1 +1,16 @@
-"use strict"
+"use strict";
+
+function setUserSettings(ev) {
+  ev.preventDefault();
+  console.log(ev);
+}
+
+function changeBgc(color) {
+  const elBody = document.querySelector(`body`);
+  elBody.style.backgroundColor = color;
+}
+
+function changeTxtColor(color) {
+  const elBody = document.querySelector(`body`);
+  elBody.style.color = color;
+}
