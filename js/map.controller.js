f83571b315469603fea2c05423b8415e0ed82d45
Uri Baynesay <uri552@gmail.com>
2022-04-08 15:07:00 +0300

    added the map to the page

--- js/map.controller.js
+++ js/map.controller.js
@@ -8,4 +8,19 @@ function init(){
         elBody.style.backgroundColor=userSettings.bkcColor;
         elBody.style.color=userSettings.txtColor;
     }
+
+    renderMap();
+}
+
+function renderMap(){
+    const elMap=document.querySelector('.map');
+    const options = {
+        center: { lat :29.572127503903275, lng: 34.955430113139926 },
+        zoom: 10
+    };
+
+    const map = new google.maps.Map(
+        elMap,
+        options
+    );
 }
