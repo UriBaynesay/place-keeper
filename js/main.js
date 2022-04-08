934c7853c776225c0f34957a10dda392cdba6e3b
Uri Baynesay <uri552@gmail.com>
2022-04-08 12:14:10 +0300

    added adjusment for user setting in index page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict"
+
+function init(){
+    const elBody=document.body;
+    debugger
+    const userSettings=getUserSettings();
+    if(userSettings){
+        elBody.style.backgroundColor=userSettings.bkcColor;
+        elBody.style.color=userSettings.txtColor;
+    }
+}
--- js/services/user.service.js
+++ js/services/user.service.js
@@ -1,6 +1,11 @@
 "use strict"
 
+const DATA_KEY='USER_SETT_DB';
 let userSettings;
+
+function getUserSettings(){
+    return _loadUserSettings();
+}
 
 function setUserSettings(email,age,bkcColor,txtColor,birthDate,birthTime=0){
     if(isNaN(birthTime)){
@@ -8,4 +13,13 @@ function setUserSettings(email,age,bkcColor,txtColor,birthDate,birthTime=0){
     }else{
         userSettings={email,age,bkcColor,txtColor,birthTimeStamp:(birthDate+birthTime)}
     }
+    _saveUserSettings();
+}
+
+function _saveUserSettings(){
+    saveToStorage(DATA_KEY,userSettings);
+}
+
+function _loadUserSettings(){
+    return loadFromStorage(DATA_KEY)
 }
